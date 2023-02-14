5408196f3d8921150d5c30a239682f4cf8bd72a9
Pattrigue <pfas19@student.aau.dk>
2023-02-14 18:06:44 +0100

    Setup class declaration

---
+++ NumberSeriesAssignment/NumberSeries.h
@@ -0,0 +1,21 @@
+﻿#ifndef NUMBERSERIESASSIGNMENT_NUMBERSERIES_H
+#define NUMBERSERIESASSIGNMENT_NUMBERSERIES_H
+
+#include <vector>
+
+class NumberSeries {
+public:
+    static void make_random(size_t size);
+    int min() const;
+    int max(); const;
+    
+    NumberSeries operator+(const NumberSeries& rhs);
+    NumberSeries& operator+=(const NumberSeries& rhs);
+    bool operator<(const NumberSeries& rhs);
+private:
+    std::vector<int> m_series;
+    int m_min;
+    int m_max;
+};
+
+#endif
