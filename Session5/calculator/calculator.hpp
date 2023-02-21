e87ad7d5c38ac1e6805b42c27d156703c19ee13a
Pattrigue <pfas19@student.aau.dk>
2023-02-21 11:32:36 +0100

    Update calculator.hpp

--- Session5/calculator/calculator.hpp
+++ Session5/calculator/calculator.hpp
@@ -53,18 +53,26 @@ namespace calculator {
     struct expr_t {
         std::unique_ptr<var_t> var;
         std::vector<std::unique_ptr<expr_t>> operands;
+        
         enum op_t {
             plus, minus, add, sub, mul, div, assign
         } op;
 
-        expr_t(const expr_t &other) { *this = other; }
+        expr_t(const expr_t &other) { 
+            *this = other; 
+        }
 
         expr_t &operator=(const expr_t &other) {
             op = other.op;
             var = other.var ? std::make_unique<var_t>(*other.var) : nullptr;
+            
             operands.resize(other.operands.size());
+            
             std::transform(other.operands.begin(), other.operands.end(), operands.begin(),
-                           [](const auto &ptr) { return std::make_unique<expr_t>(*ptr); });
+                           [](const auto &ptr) { 
+                return std::make_unique<expr_t>(*ptr); 
+            });
+            
             return *this;
         }
 
@@ -74,9 +82,11 @@ namespace calculator {
 
         ~expr_t() noexcept = default;
 
-        expr_t(const var_t &v, op_t op = plus) : var{std::make_unique<var_t>(v)}, op{op} {}
+        explicit expr_t(const var_t &v, op_t op = plus)
+                : var{std::make_unique<var_t>(v)}, op{op} {}
 
-        expr_t(const var_t &v, const expr_t &e) : var{std::make_unique<var_t>(v)}, operands(1), op{assign} {
+        expr_t(const var_t &v, const expr_t &e)
+                : var{std::make_unique<var_t>(v)}, operands(1), op{assign} {
             operands[0] = std::make_unique<expr_t>(e);
         }
 
@@ -84,7 +94,8 @@ namespace calculator {
             operands[0] = std::make_unique<expr_t>(e);
         }
 
-        expr_t(const expr_t &e1, const expr_t &e2, op_t op) : operands(2), op{op} {
+        expr_t(const expr_t &e1, const expr_t &e2, op_t op) 
+                : operands(2), op{op} {
             operands[0] = std::make_unique<expr_t>(e1);
             operands[1] = std::make_unique<expr_t>(e2);
         }
@@ -92,46 +103,62 @@ namespace calculator {
         double operator()(state_t &s) const {
             if (var) {
                 auto &v = *var;
+                
                 switch (op) {
                     case expr_t::plus:
                         return v(s);
                     case expr_t::minus:
                         return -v(s);
                     case expr_t::assign:
-                        if (operands.empty())
+                        if (operands.empty()) {
                             throw std::logic_error{"missing expression to evaluate"};
+                        }
+                        
                         return v(s, *operands[0]);
                     default:
                         throw std::logic_error{"unsupported operation over a variable"};
                 }
             } else {
                 auto &e1 = *operands[0];
+                
                 switch (op) {
                     case expr_t::plus:
-                        if (operands.empty())
+                        if (operands.empty()) {
                             throw std::logic_error{"bug: missing an operand for unary +"};
+                        }
+                        
                         return e1(s);
                     case expr_t::minus:
-                        if (operands.empty())
+                        if (operands.empty()) {
                             throw std::logic_error{"bug: missing an operand for unary -"};
+                        }
+                        
                         return -e1(s);
                     case expr_t::add: {
-                        if (operands.size() != 2)
+                        if (operands.size() != 2) {
                             throw std::logic_error{"bug: expecting two operands for binary +"};
+                        }
+                        
                         auto &e2 = *operands[1];
+                        
                         return e1(s) + e2(s);
                     }
                     case expr_t::sub: {
-                        if (operands.size() != 2)
+                        if (operands.size() != 2) {
                             throw std::logic_error{"bug: expecting two operands for binary -"};
+                        }
+                        
                         auto &e2 = *operands[1];
+                        
                         return e1(s) - e2(s);
                     }
                     case expr_t::mul: {
                         if (operands.size() != 2) {
                             throw std::logic_error{"bug: expecting two operands for binary *"};
                         }
+                        
                         auto &e2 = *operands[1];
+                        
                         return e1(s) * e2(s);
                     }
                     case expr_t::div: {
@@ -144,6 +171,7 @@ namespace calculator {
                         if (e2(s) == 0) {
                             throw std::logic_error{"division by zero"};
                         }
+                        
                         return e1(s) / e2(s);
                     }
                     default:
@@ -154,23 +182,39 @@ namespace calculator {
     };
 
     /** assignment operation */
-    inline double var_t::operator()(state_t &s, const expr_t &e) const { return s[id] = e(s); }
+    inline double var_t::operator()(state_t &s, const expr_t &e) const {
+        return s[id] = e(s); 
+    }
 
     /** unary operators: */
-    inline expr_t operator+(const expr_t &e) { return expr_t{e, expr_t::plus}; }
+    inline expr_t operator+(const expr_t &e) { 
+        return expr_t{e, expr_t::plus}; 
+    }
 
-    inline expr_t operator-(const expr_t &e) { return expr_t{e, expr_t::minus}; }
+    inline expr_t operator-(const expr_t &e) { 
+        return expr_t{e, expr_t::minus}; 
+    }
 
     /** binary operators: */
-    inline expr_t operator+(const expr_t &e1, const expr_t &e2) { return expr_t{e1, e2, expr_t::add}; }
+    inline expr_t operator+(const expr_t &e1, const expr_t &e2) { 
+        return expr_t{e1, e2, expr_t::add}; 
+    }
 
-    inline expr_t operator-(const expr_t &e1, const expr_t &e2) { return expr_t{e1, e2, expr_t::sub}; }
+    inline expr_t operator-(const expr_t &e1, const expr_t &e2) {
+        return expr_t{e1, e2, expr_t::sub}; 
+    }
     
-    inline expr_t operator*(const expr_t &e1, const expr_t &e2) { return expr_t{e1, e2, expr_t::mul}; }
+    inline expr_t operator*(const expr_t &e1, const expr_t &e2) { 
+        return expr_t{e1, e2, expr_t::mul}; 
+    }
     
-    inline expr_t operator/(const expr_t &e1, const expr_t &e2) { return expr_t{e1, e2, expr_t::div}; }
+    inline expr_t operator/(const expr_t &e1, const expr_t &e2) {
+        return expr_t{e1, e2, expr_t::div}; 
+    }
 
-    inline expr_t operator<<=(const var_t &v, const expr_t &e) { return expr_t{v, e}; }
+    inline expr_t operator<<=(const var_t &v, const expr_t &e) { 
+        return expr_t{v, e}; 
+    }
 }
 
 #endif // INCLUDE_ALGEBRA_HPP
