e2d33e137fd626187e2f709dd2dcb182e3a36233
Pattrigue <patrick@gamehouse.dk>
2023-03-28 09:48:03 +0200

    mtqueue exercise

--- Concurrency/include/mtqueue.hpp
+++ Concurrency/include/mtqueue.hpp
@@ -2,20 +2,35 @@
 #define CONCURRENCY_MTQUEUE_H
 
 #include <queue>
+#include <mutex>
+#include <optional>
 
 template <typename T>
 class mtqueue {
 	std::queue<T> container; // TODO: benchmark other containers
-	// TODO: add multi-threading safety (MT-safety)
+    std::mutex mutex;
 public:
 	void put(T&& item) {
-		// TODO: implement putting the element into container
+        std::lock_guard<std::mutex> lock(mutex);
+        container.push(std::move(item));
 	}
+    
 	void put(const T& item) {
-		// TODO: implement putting the element into container
+        std::lock_guard<std::mutex> lock(mutex);
+        container.push(item);
 	}
-	T get(){
-		// TODO: implement fetching an element from a container
+    
+	std::optional<T> get() {
+        std::lock_guard<std::mutex> lock(mutex);
+        
+        if (container.empty()) {
+            return std::nullopt;
+        }
+        
+        T item = container.front();
+        container.pop();
+        
+        return item;
 	}
 };
 
--- Concurrency/src/mtqueue.cpp
+++ Concurrency/src/mtqueue.cpp
@@ -1,18 +1,57 @@
+#include <thread>
+#include <iostream>
+#include <random>
 #include "mtqueue.hpp"
 
-void produce(mtqueue<int>&)
-{
-	// TODO: create producer routine: put random numbers into queue
+std::atomic<bool> done = false;
+
+void produce(mtqueue<int>& q) {
+	// Producer routine: put random numbers into queue
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> values_dist(0, 100);
+    std::uniform_int_distribution<> sleep_dist(0, 500);
+    
+    while (!done) {
+        q.put(values_dist(gen));
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_dist(gen)));
+    }
 }
 
-void consume(mtqueue<int>&)
-{
-	// TODO: create consumer routine: fetch and print values
+void consume(mtqueue<int>& q) {
+    // Consumer routine: fetch and print values
+    while (!done) {
+        std::optional<int> item = q.get();
+        
+        if (!item.has_value()) {
+            continue;
+        }
+        
+        std::cout << "Consumed " << item.value() << "." << std::endl;
+    }
 }
 
 int main(){
-	// TODO: create an instance of a queue
-	// TODO: spawn multiple threads for producer and consumer using the queue
+	// Create an instance of a queue
+    mtqueue<int> q;
+    
+	// Spawn multiple threads for producer and consumer using the queue
+    std::vector<std::jthread> threads;
+    
+    for (int i = 0; i < 10; ++i) {
+        threads.emplace_back(produce, std::ref(q));
+        threads.emplace_back(consume, std::ref(q));
+    }
+    
+    // Let the threads run for 5 seconds
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    done = true;
+    
+    // Wait for the threads to finish
+    for (auto& thread : threads) {
+        thread.join();
+    }
+    
     // NOTE: producer and consumer expect to bind to the queue by reference:
     // 1) STL uses values, therefore plain queue argument might not work: wrap the queue into std::ref instead.
     // 2) Using std::ref is not normal: it is a red-flag that we are doing something wrong.
